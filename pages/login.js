c415bcecff2cf2832bf56d4dd3220b848a86d15a
yash gupta <yash3144@gmail.com>
2021-12-01 19:07:50 +0530

    Create login and register page

---
+++ pages/login.js
@@ -0,0 +1,6 @@
+import Layout from "../components/Layout";
+
+const  Login = () => <Layout>Login Page</Layout>
+
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,6 @@
+import Layout from "../components/Layout";
+
+const  Register = () => <Layout>Register Page</Layout>
+
+
+export default Register;
