5a78c7999b1df5a0944fc52dae82255899844aa6
yash gupta <yash3144@gmail.com>
2021-12-01 19:32:01 +0530

    Implement nprogress package

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,27 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react'
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = url => NProgress.start()
+Router.onRouteChangeComplete = url => NProgress.done()
+Router.onRouteChangeError = url => NProgress.done()
 
 const Layout = ({ children }) => {
     const head = () => (
-
+        
+        <React.Fragment>
             <link 
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
                 rel="stylesheet" 
                 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
                 crossorigin="anonymous" 
             />
+            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
+        </React.Fragment>
+
     );
 
 
