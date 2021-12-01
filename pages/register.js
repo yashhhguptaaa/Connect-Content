c9033ef816e61e7654a64ca4a2a710dd5460c449
yash gupta <yash3144@gmail.com>
2021-12-01 20:27:08 +0530

    Create Form in Register Page

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,79 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 
-const  Register = () => <Layout>Register Page</Layout>
+const Register = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: "",
+    buttonText: "Register",
+  });
 
+  const { name, email, password, error, success, buttonText } = state;
+
+  const handleChange = (name) => (e) => {
+    setState({
+      ...state,
+      [name]: e.target.value,
+      error: "",
+      success: "",
+      buttonText: "Register",
+    });
+  };
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      console.table({name, email, password})
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          value={name}
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control mb-3"
+          placeholder="Type your name"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          value={email}
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control mb-3"
+          placeholder="Type your email"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          value={password}
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control mb-3"
+          placeholder="Type your password"
+        />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-outline-danger">{buttonText}</button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <h1>Register</h1>
+        <br />
+        {registerForm()}
+        <br />
+        {JSON.stringify(state)}
+      </div>
+    </Layout>
+  );
+};
 
 export default Register;
