0e54f262286cadb6c103da4ee4d6887647b9a66d
yash gupta <yash3144@gmail.com>
2021-12-02 16:29:18 +0530

    Apply authentication for registration in backend using express-validator

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const {userRegisterValidator} = require("../validators/auth");
+const {runValidation} = require("../validators");
+
 const {register} = require('../controllers/auth')
 
-router.post('/register',register);
+router.post('/register', userRegisterValidator, runValidation, register);
 
 module.exports =router;
