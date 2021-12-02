c4bff30043cb795827e8f99130bcad3d521247f8
yash gupta <yash3144@gmail.com>
2021-12-02 17:03:40 +0530

    Connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const morgan= require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
-const authRoutes = require('./routes/auth')
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch(() => console.log("some Error"));
+
+const authRoutes = require("./routes/auth");
 
 //app middlewares
-app.use(morgan('dev'));
-app.use(bodyParser.json())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 // app.use(cors())
-app.use(cors({origin: process.env.CLIENT_URL}));
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
-app.use('/api',authRoutes)
+app.use("/api", authRoutes);
 
-
-const port = process.env.PORT || 8000
-app.listen(port, () => console.log(`Server is running on port : ${port}`))
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port : ${port}`));
