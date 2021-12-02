55ca4bc26e2e51d0771f074869ca2f41fdfeedeb
yash gupta <yash3144@gmail.com>
2021-12-02 15:06:21 +0530

    Create in userSchema virtual and methods

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
-const mongoose  = require('mongoose')
+const mongoose  = require('mongoose');
+const crypto = require('crypto')
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -38,4 +39,39 @@ const userSchema = new mongoose.Schema({
         default:''
     }
 }, {timestamps:true});
+
+// Virtual fields
+userSchema.virtual('password')
+    .set(function(password){
+        // create temp varibale called _password
+        this._password =password
+        // generate salt
+        this.salt = makeSalt()
+        // encrypt password
+        this.hashed_password = this.encryptPassword(password)  
+    })
+    .get(function(){
+        return this._password
+    })
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    encryptPassword : function(password) {
+        if(!password) return ''
+        try{
+            return crypto.createHmac('sha1', this.salt)
+                    .update(password)
+                    .digest('hex')
+        }catch(err) {
+            return ''
+        }
+    },
+    makeSalt = function() {
+        return Math.round(new Date().valueOf() * Math.random()) + ''
+    }
+};
+
+module.exports = mongoose.model('User',userSchema);
 
