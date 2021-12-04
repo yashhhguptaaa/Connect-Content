aab3a1d18e8eff985230279b4638281c2de5ec72
yash gupta <yash3144@gmail.com>
2021-12-04 22:04:36 +0530

    Minor fix

--- server/models/user.js
+++ server/models/user.js
@@ -1,77 +1,84 @@
-const mongoose  = require('mongoose');
-const crypto = require('crypto')
+const mongoose = require('mongoose');
+const crypto = require('crypto');
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        trim: true,
-        required : true,
-        max : 12,
-        unique: true,
-        index: true,
-        lowercase: true
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 12,
+            unique: true,
+            index: true,
+            lowercase: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 32
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        salt: String,
+        role: {
+            type: String,
+            default: 'subscriber'
+        },
+        resetPasswordLink: {
+            data: String,
+            default: ''
+        }
     },
-    name: {
-        type: String,
-        trim: true,
-        required : true,
-        max : 32
-    },
-    email: {
-        type: String,
-        trim: true,
-        required : true,
-        unique: true,
-        max : 32,
-        lowercase: true
-    },
-    hashed_password: {
-        type: String,
-        required : true
-    },
-    salt : String,
-    role :{
-        type: String,
-        default : 'subscriber'
-    },
-    resetPasswordLink :{
-        data: String,
-        default:''
-    }
-}, {timestamps:true});
+    { timestamps: true }
+);
 
-// Virtual fields
-userSchema.virtual('password')
-    .set(function(password){
-        // create temp varibale called _password
-        this._password =password
+// virtual fields
+userSchema
+    .virtual('password')
+    .set(function(password) {
+        // create temp variable called _password
+        this._password = password;
         // generate salt
-        this.salt = makeSalt()
+        this.salt = this.makeSalt();
         // encrypt password
-        this.hashed_password = this.encryptPassword(password)  
+        this.hashed_password = this.encryptPassword(password);
     })
-    .get(function(){
-        return this._password
-    })
+    .get(function() {
+        return this._password;
+    });
 
+// methods > authenticate, encryptPassword, makeSalt
 userSchema.methods = {
     authenticate: function(plainText) {
         return this.encryptPassword(plainText) === this.hashed_password;
     },
-    encryptPassword : function(password) {
-        if(!password) return ''
-        try{
-            return crypto.createHmac('sha1', this.salt)
-                    .update(password)
-                    .digest('hex')
-        }catch(err) {
-            return ''
+
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
         }
     },
-    makeSalt : function() {
-        return Math.round(new Date().valueOf() * Math.random()) + ''
+
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
     }
 };
+// export user model
 
-module.exports = mongoose.model('User',userSchema);
-
+module.exports = mongoose.model('User', userSchema);
