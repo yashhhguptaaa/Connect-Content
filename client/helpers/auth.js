651d8c26d544496ae7531af74285c09b57bd3d09
yash gupta <yash3144@gmail.com>
2021-12-05 16:26:05 +0530

    Add logout fn

--- client/helpers/auth.js
+++ client/helpers/auth.js
@@ -1,4 +1,5 @@
 import cookie from 'js-cookie';
+import Router  from 'next/router';
 
 
 // set in cookie
@@ -61,4 +62,10 @@ export const isAuth = () => {
              }
          }
      }
+}
+
+export const logout = () => {
+    removeCookie('token');
+    removeLocalStorage('user');
+    Router.push('/login')
 }
