e840fd49171f1e5f6290653282b657be535f1ab5
yash gupta <yash3144@gmail.com>
2021-12-05 12:43:39 +0530

    Create validator for login

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -12,3 +12,13 @@ exports.userRegisterValidator = [
         .isLength({ min:6 })
         .withMessage('Password must be at least 6 characters long')
 ]
+
+exports.userLoginValidator = [
+    check('email')
+        .isEmail()
+        .withMessage('Must be a valid email address'),
+    check('password')
+        .isLength({ min:6 })
+        .withMessage('Password must be at least 6 characters long')
+]
+
