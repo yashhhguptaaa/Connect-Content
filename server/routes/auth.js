7541e53298fe912bd809b14746e6b53c943f427d
yash gupta <yash3144@gmail.com>
2021-12-05 12:43:14 +0530

    Add login router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const {userRegisterValidator} = require("../validators/auth");
+const {userRegisterValidator, userLoginValidator} = require("../validators/auth");
 const {runValidation} = require("../validators");
 
-const {register, registerActivate} = require('../controllers/auth')
+const {register, registerActivate, login} = require('../controllers/auth')
 
 router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/register/activate', registerActivate);
+router.post('/login', userLoginValidator, runValidation, login);
 
 module.exports =router;
