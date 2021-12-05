1c16e4d0364fa4f9548240b7cc6529f2fe9cec56
yash gupta <yash3144@gmail.com>
2021-12-05 16:48:07 +0530

    Add conditional statements

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,60 +1,81 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import React from 'react'
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import {isAuth, logout} from '../helpers/auth';
-import 'nprogress/nprogress.css'
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { isAuth, logout } from "../helpers/auth";
+import "nprogress/nprogress.css";
 
-Router.onRouteChangeStart = url => NProgress.start()
-Router.onRouteChangeComplete = url => NProgress.done()
-Router.onRouteChangeError = url => NProgress.done()
+Router.onRouteChangeStart = (url) => NProgress.start();
+Router.onRouteChangeComplete = (url) => NProgress.done();
+Router.onRouteChangeError = (url) => NProgress.done();
 
 const Layout = ({ children }) => {
-    const head = () => (
-        
-        <React.Fragment>
-            <link 
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-                rel="stylesheet" 
-                integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-                crossOrigin="anonymous" 
-            />
-            <link rel="stylesheet" href="/static/css/styles.css" />
-            
-        </React.Fragment>
-
-    );
-
-
-    const nav = () => (
-        <ul className="nav nav-tabs bg-warning">
-            <li className="nav-item ">
-                <Link  href="/">
-                    <a className="nav-link text-dark">Home</a>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link href="/login">
-                    <a className="nav-link text-dark" >Login</a>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link href="/register">
-                    <a className="nav-link text-dark" >Register</a>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link text-dark" onClick={logout}>Logout</a>
-            </li>
-        </ul>
-    )
-    return <React.Fragment>
-        {head()}{nav()}
-        <div className="container pt-5 pb-5">
-        {children}
-        </div>
+  const head = () => (
+    <React.Fragment>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossOrigin="anonymous"
+      />
+      <link rel="stylesheet" href="/static/css/styles.css" />
     </React.Fragment>
-}
+  );
 
-export default Layout
+  const nav = () => (
+    <ul className="nav nav-tabs bg-warning">
+      <li className="nav-item ">
+        <Link href="/">
+          <a className="nav-link text-dark">Home</a>
+        </Link>
+      </li>
+
+      {!isAuth() && (
+        <React.Fragment>
+          <li className="nav-item">
+            <Link href="/login">
+              <a className="nav-link text-dark">Login</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/register">
+              <a className="nav-link text-dark">Register</a>
+            </Link>
+          </li>
+        </React.Fragment>
+      )}
+
+      {isAuth() && isAuth().role === "admin" && (
+        <li className="nav-item ms-auto">
+          <Link href="/admin">
+            <a className="nav-link text-dark">{isAuth().name}</a>
+          </Link>
+        </li>
+      )}
+      {isAuth() && isAuth().role === "subscriber" && (
+        <li className="nav-item ms-auto">
+          <Link href="/user">
+            <a className="nav-link text-dark">{isAuth().name}</a>
+          </Link>
+        </li>
+      )}
+      {isAuth() && (
+        <li className="nav-item">
+          <a className="nav-link text-dark" onClick={logout}>
+            Logout
+          </a>
+        </li>
+      )}
+    </ul>
+  );
+  return (
+    <React.Fragment>
+      {head()}
+      {nav()}
+      <div className="container pt-5 pb-5">{children}</div>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
