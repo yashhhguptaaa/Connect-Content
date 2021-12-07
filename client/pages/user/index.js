05d598fde28ab0ea12a0be9c0b1fb30278e52847
yash gupta <yash3144@gmail.com>
2021-12-07 20:43:50 +0530

    User restriction

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,4 +1,30 @@
 import Layout from "../../components/Layout";
+import axios from "axios";
+import { API } from "../../config";
+import { getCookie } from "../../helpers/auth";
 
-const User = () => <Layout><h1>User Page</h1></Layout>
+const User = ({ user }) => (
+  <Layout>
+    <p>{JSON.stringify(user)}</p>
+  </Layout>
+);
+
+User.getInitialProps = async (context) => {
+  const token = getCookie("token", context.req);
+  try {
+    const response = await axios.get(`${API}/user`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+        contentType: "application/json",
+      },
+    });
+
+    return { user: response.data };
+  } catch (error) {
+    if (error.response.status === 401) {
+      return { user: "no user" };
+    }
+  }
+};
+
 export default User;
