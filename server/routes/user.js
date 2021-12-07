7dcdf3da0441bbb5437f1e1bca3f778c8ba4d96f
yash gupta <yash3144@gmail.com>
2021-12-07 14:56:41 +0530

    Create user route

---
+++ server/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// import middlewares
+const { requireSignin, authMiddleware, adminMiddleware } = require('../controllers/auth');
+
+// import controllers
+const { read } = require('../controllers/user');
+
+// routes
+router.get('/user', requireSignin, authMiddleware, read);
+router.get('/admin', requireSignin, adminMiddleware, read);
+
+module.exports = router;
