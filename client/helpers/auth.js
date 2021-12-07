755d6ddc5c7b2aa9e45633208732bc511c426331
yash gupta <yash3144@gmail.com>
2021-12-07 20:43:32 +0530

    getCookieFrom Browser and getCookie From Server

--- client/helpers/auth.js
+++ client/helpers/auth.js
@@ -22,10 +22,30 @@ export const removeCookie = (key) => {
 
 // get from cookie such as stored tokens
 // will be useful when we need to make request to server wuth auth token
-export const getCookie = key => {
-    if(process.browser){
-        return cookie.get(key)
+export const getCookie = (key, req) => {
+    // if(process.browser){
+    //     return cookie.get(key)
+    // }
+
+    return process.browser ? getCookieFromBrowser(key) : getCookieFromServer(key, req);
+}
+
+export const getCookieFromBrowser = (key) => {
+    return cookie.get(key)
+}
+
+export const getCookieFromServer = (key, req) => {
+    if(!req.headers.cookie) {
+        return undefined
     }
+    console.log("req.headers.cookie : : ",req.headers.cookie)
+    let token = req.headers.cookie.split(';').find(c => c.trim().startsWith(`${key}=`))
+    if(!token) {
+        return undefined
+    }
+    let tokenValue = token.split('=')[1]
+    console.log("getCookieFromServer" , tokenValue)
+    return tokenValue
 }
 
 // set in localstorage
