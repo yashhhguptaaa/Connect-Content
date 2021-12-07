d0221b486abf261b6ab2f04c9de0ccd258910d54
yash gupta <yash3144@gmail.com>
2021-12-07 14:57:28 +0530

    create read in user controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,10 +10,4 @@ router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/register/activate', registerActivate);
 router.post('/login', userLoginValidator, runValidation, login);
 
-// router.get('/secret',requireSignIn, (req,res) => {
-//     res.json({
-//         data : 'This is secret page for logged in users only'
-//     });
-// });
-
 module.exports =router;
