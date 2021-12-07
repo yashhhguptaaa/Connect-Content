6ffcecb1b2311e8f820142531f539775da933011
yash gupta <yash3144@gmail.com>
2021-12-07 22:55:50 +0530

    Remove getinitialProps

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -2,29 +2,13 @@ import Layout from "../../components/Layout";
 import axios from "axios";
 import { API } from "../../config";
 import { getCookie } from "../../helpers/auth";
+import withUser from "../withUser";
 
-const User = ({ user }) => (
+const User = ({ user, token }) => (
   <Layout>
     <p>{JSON.stringify(user)}</p>
   </Layout>
 );
 
-User.getInitialProps = async (context) => {
-  const token = getCookie("token", context.req);
-  try {
-    const response = await axios.get(`${API}/user`, {
-      headers: {
-        authorization: `Bearer ${token}`,
-        contentType: "application/json",
-      },
-    });
 
-    return { user: response.data };
-  } catch (error) {
-    if (error.response.status === 401) {
-      return { user: "no user" };
-    }
-  }
-};
-
-export default User;
+export default withUser(User);
