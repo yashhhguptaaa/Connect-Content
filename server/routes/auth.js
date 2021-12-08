8e3d11cad9b1d2e0b3385ea38b699ecb709f94d0
yash gupta <yash3144@gmail.com>
2021-12-08 14:01:33 +0530

    route for forgotPassword

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const {userRegisterValidator, userLoginValidator} = require("../validators/auth");
+const {userRegisterValidator, userLoginValidator, forgetPasswordValidator, resetPasswordValidator} = require("../validators/auth");
 const {runValidation} = require("../validators");
 
-const {register, registerActivate, login, requireSignIn} = require('../controllers/auth')
+const {register, registerActivate, login, requireSignIn, forgetPassword, resetPassword} = require('../controllers/auth')
 
 router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/register/activate', registerActivate);
 router.post('/login', userLoginValidator, runValidation, login);
+router.put('/forgot-password', forgetPasswordValidator, runValidation, forgetPassword);
+router.put('/forgot-password', resetPasswordValidator, runValidation, resetPassword);
 
 module.exports =router;
