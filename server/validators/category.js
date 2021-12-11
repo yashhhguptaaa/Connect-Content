daf3755e79f68ace603cececd234e46a78af7feb
yash gupta <yash3144@gmail.com>
2021-12-11 16:51:47 +0530

    validations in order to access category controllers

---
+++ server/validators/category.js
@@ -0,0 +1,12 @@
+const { check } = require("express-validator");
+
+exports.categoryCreateValidator = [
+  check("name").not().isEmpty().withMessage("Name is required"),
+  check("image").isEmpty().withMessage("Image is required"),
+  check("content").isLength({ min: 20 }).withMessage("Content is required"),
+];
+
+exports.categoryUpdateValidator = [
+    check("name").not().isEmpty().withMessage("Name is required"),
+    check("content").isLength({ min: 20 }).withMessage("Content is required"),
+  ];
