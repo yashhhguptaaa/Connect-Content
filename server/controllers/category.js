dd79ccc613e2d0c989a33d35bccad1f0fe3b9d8f
yash gupta <yash3144@gmail.com>
2021-12-12 00:11:59 +0530

    Upload image in aws S3

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,32 +1,70 @@
-const Category = require('../models/category')
-const slugify = require('slugify')
+const Category = require("../models/category");
+const slugify = require("slugify");
+const formidable = require("formidable");
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
+//s3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 exports.create = (req, res) => {
-    const {name, content} = req.body
-    const slug = slugify(name)
-    const image = {
-        url : `https://via.placeholder.com/200X150.png?text=${process.env.CLIENT_URL}`,
-        key : '123'
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not upload",
+      });
+    }
+    // console.table({err, fields, files})
+    const { name, content } = fields;
+    const { image } = files;
+
+    const slug = slugify(name);
+    let category = new Category({ name, content, slug });
+
+    if (image.size > 2000000) {
+      return res.status(400).json({
+        error: "Image should be less than 2mb",
+      });
+    }
+
+    // upload image to s3
+    const params = {
+      Bucket: "connect-content1",
+      Key: `category/${uuidv4()}`,
+      Body: fs.readFileSync(image.filepath),
+      ACL: "public-read",
+      ContentType: "image/jpg",
     };
 
-    const category = new Category({name, slug, image, content})
-    category.postedBy = req.user._id
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.log("err :", err);
+        res.status(400).json({ error: "Upload to s3 failed" });
+      }
 
-    category.save((err, data) => {
-        if(err) {
-            console.log('CATEGORY CREATE ERR', err)
-            return res.status(400).json({
-                error : 'Category create failed'
-            })
+      console.log("AWS UPLOAD RES DATA", data);
+      category.image.url = data.Location;
+      category.image.key = data.key;
+
+      // save to db
+      category.save((err, success) => {
+        if (err) {
+            console.log("Error while post category: ",err)
+          res.status(400).json({ error: "Duplicate Category" });
         }
-
-        res.json(data)
-    })
+        return res.json(success);
+      });
+    });
+  });
 };
 
 exports.list = (req, res) => {};
 exports.read = (req, res) => {};
 exports.update = (req, res) => {};
 exports.remove = (req, res) => {};
-
