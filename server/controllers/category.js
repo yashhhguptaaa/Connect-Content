f83b760d4a269ba52f0d2227db3372d102184974
yash gupta <yash3144@gmail.com>
2021-12-14 12:47:48 +0530

    define read function in categories controller

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Link = require("../models/link");
 const slugify = require("slugify");
 const formidable = require("formidable");
 const AWS = require("aws-sdk");
@@ -12,7 +13,7 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
 });
 
- /* exports.create = (req, res) => {
+/* exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.parse(req, (err, fields, files) => {
     if (err) {
@@ -64,15 +65,17 @@ const s3 = new AWS.S3({
   });
 }; */
 
-
 exports.create = (req, res) => {
-  const {name, image, content} = req.body
+  const { name, image, content } = req.body;
 
   //image data
-  const base64Data = new Buffer.from(image.replace(/^data:image\/\w+;base64,/,''),'base64')
+  const base64Data = new Buffer.from(
+    image.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  );
   /* "^data:image" replaces with '' + ";base64," replaces with '' and  the type is 'base64' */
 
-  const type = image.split(';')[0].split('/')[1];
+  const type = image.split(";")[0].split("/")[1];
 
   const slug = slugify(name);
   let category = new Category({ name, content, slug });
@@ -83,48 +86,74 @@ exports.create = (req, res) => {
     Key: `category/${uuidv4()}.${type}`,
     Body: base64Data,
     ACL: "public-read",
-    contentEncoding: 'base64',
+    contentEncoding: "base64",
     ContentType: `image/${type}`,
   };
 
   s3.upload(params, function (err, data) {
     if (err) {
-      console.log("err :", err); 
+      console.log("err :", err);
       res.status(400).json({ error: "Upload to s3 failed" });
     }
 
     console.log("AWS UPLOAD RES DATA", data);
     category.image.url = data.Location;
     category.image.key = data.key;
-    
+
     // posted By
     category.postedBy = req.user._id;
 
     // save to db
     category.save((err, success) => {
       if (err) {
-          console.log("Error while post category: ",err)
+        console.log("Error while post category: ", err);
         res.status(400).json({ error: "Duplicate Category" });
       }
       return res.json(success);
     });
   });
-
 };
 
 exports.list = (req, res) => {
+  Category.find({}).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Categories could not load",
+      });
+    }
+    res.json(data);
+  });
+};
 
-  Category.find({}).exec((err,data) => {
-     if(err) {
-       return res.status(400).json({
-         error : 'Categories could not load'
-       })
-     }
-     res.json(data)
+exports.read = (req, res) => {
+  const { slug} = req.params;
+  let limit = req.body.limit ? parseInt(req.body.limit) : 5
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0
+
+  Category.findOne({ slug }).populate("postedBy", "_id name username")
+  .exec((err, category) => {
+      if(err) {
+        return res.status(400).json({
+          error : 'Could not load category'
+        });
+      }
+      Link.find({categories : category})
+      .populate('postedBy','_id name username')
+      .populate('categories','name')
+      .sort({createdAt: -1})
+      .limit(limit)
+      .skip(skip)
+      .exec((err, links) => {
+        if(err) {
+          return res.status(400).json({
+            error : 'Could not load links of a category'
+          });
+        }
+        res.json({category, links});
+
+      })
   })
 };
 
-
-exports.read = (req, res) => {};
 exports.update = (req, res) => {};
 exports.remove = (req, res) => {};
