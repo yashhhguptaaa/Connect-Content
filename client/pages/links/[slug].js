b8335e7bca2d21a5ac8bc24781b8ca1829e05a6e
yash gupta <yash3144@gmail.com>
2021-12-19 20:51:16 +0530

    Implement infinite scrolling on links page using react-infinite-scroller

--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -7,6 +7,7 @@ import axios from "axios";
 import { API } from "../../config";
 import renderHTML from "react-render-html";
 import moment from "moment";
+import InfiniteScroll from 'react-infinite-scroller';
 
 const Links = ({
   query,
@@ -79,16 +80,16 @@ const Links = ({
     setSkip(toSkip);
   };
 
-  const loadMoreButton = () => {
-    return (
-      size > 0 &&
-      size >= limit && (
-        <button onClick={loadMore} className="btn btn-outline-primary btn-lg">
-          Load More
-        </button>
-      )
-    );
-  };
+  // const loadMoreButton = () => {
+  //   return (
+  //     size > 0 &&
+  //     size >= limit && (
+  //       <button onClick={loadMore} className="btn btn-outline-primary btn-lg">
+  //         Load More
+  //       </button>
+  //     )
+  //   );
+  // };
 
   return (
     <Layout>
@@ -115,7 +116,21 @@ const Links = ({
         </div>
       </div>
 
-      <div className="text-center pt-4 pb-5">{loadMoreButton()}</div>
+      {/* <div className="text-center pt-4 pb-5">{loadMoreButton()}</div> */}
+
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={loadMore}
+            hasMore= {size>0 && size>= limit} 
+            loader={
+              <img style={{height:"80px"}} src="/static/images/loading.gif" alt="loading" />
+            }
+          >
+          </InfiniteScroll>
+        </div>
+      </div>
     </Layout>
   );
 };
