8bfa9a989e5340e2c5769c72e07a89d9081ac9d7
yash gupta <yash3144@gmail.com>
2021-12-31 20:25:15 +0530

    Create update controller and validator for user profile update

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // import middlewares
-const { requireSignin, authMiddleware, adminMiddleware } = require('../controllers/auth');
+const {
+  requireSignin,
+  authMiddleware,
+  adminMiddleware,
+} = require("../controllers/auth");
 
 // import controllers
-const { read } = require('../controllers/user');
+const { read, update } = require("../controllers/user");
+const { userUpdateValidator } = require("../validators/auth");
+const { runValidation } = require("../validators");
 
 // routes
-router.get('/user', requireSignin, authMiddleware, read);
-router.get('/admin', requireSignin, adminMiddleware, read);
+router.get("/user", requireSignin, authMiddleware, read);
+router.get("/admin", requireSignin, adminMiddleware, read);
+router.put(
+  "/user",
+  userUpdateValidator,
+  runValidation,
+  requireSignin,
+  authMiddleware,
+  update
+);
 
 module.exports = router;
