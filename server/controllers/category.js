564413b7500906eb830c6fa33493ef3c7495559b
Yash Gupta <yash3144@gmail.com>
2023-03-31 22:18:53 +0530

    refactor: updates the bucket name and removes all commented code

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -4,7 +4,6 @@ const slugify = require("slugify");
 const formidable = require("formidable");
 const AWS = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
-// const fs = require("fs");
 
 //s3
 const s3 = new AWS.S3({
@@ -12,58 +11,6 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
-
-/* exports.create = (req, res) => {
-  let form = new formidable.IncomingForm();
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        error: "Image could not upload",
-      });
-    }
-    // console.table({err, fields, files})
-    const { name, content } = fields;
-    const { image } = files;
-
-    const slug = slugify(name);
-    let category = new Category({ name, content, slug });
-
-    if (image.size > 2000000) {
-      return res.status(400).json({
-        error: "Image should be less than 2mb",
-      });
-    }
-
-    // upload image to s3
-    const params = {
-      Bucket: "connect-content1",
-      Key: `category/${uuidv4()}`,
-      Body: fs.readFileSync(image.filepath),
-      ACL: "public-read",
-      ContentType: "image/jpg",
-    };
-
-    s3.upload(params, function (err, data) {
-      if (err) {
-        console.log("err :", err);
-        res.status(400).json({ error: "Upload to s3 failed" });
-      }
-
-      console.log("AWS UPLOAD RES DATA", data);
-      category.image.url = data.Location;
-      category.image.key = data.key;
-
-      // save to db
-      category.save((err, success) => {
-        if (err) {
-            console.log("Error while post category: ",err)
-          res.status(400).json({ error: "Duplicate Category" });
-        }
-        return res.json(success);
-      });
-    });
-  });
-}; */
 
 exports.create = (req, res) => {
   const { name, image, content } = req.body;
@@ -82,7 +29,7 @@ exports.create = (req, res) => {
 
   // upload image to s3
   const params = {
-    Bucket: "connect-content1",
+    Bucket: "connect-contentv2",
     Key: `category/${uuidv4()}.${type}`,
     Body: base64Data,
     ACL: "public-read",
@@ -126,133 +73,127 @@ exports.list = (req, res) => {
 };
 
 exports.read = (req, res) => {
-  const { slug} = req.params;
-  let limit = req.body.limit ? parseInt(req.body.limit) : 5
-  let skip = req.body.skip ? parseInt(req.body.skip) : 0
+  const { slug } = req.params;
+  let limit = req.body.limit ? parseInt(req.body.limit) : 5;
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
 
-  Category.findOne({ slug }).populate("postedBy", "_id name username")
-  .exec((err, category) => {
-      if(err) {
+  Category.findOne({ slug })
+    .populate("postedBy", "_id name username")
+    .exec((err, category) => {
+      if (err) {
         return res.status(400).json({
-          error : 'Could not load category'
+          error: "Could not load category",
         });
       }
-      Link.find({categories : category})
-      .populate('postedBy','_id name username')
-      .populate('categories','name')
-      .sort({createdAt: -1})
-      .limit(limit)
-      .skip(skip)
-      .exec((err, links) => {
-        if(err) {
-          return res.status(400).json({
-            error : 'Could not load links of a category'
-          });
-        }
-        res.json({category, links});
-
-      })
-  })
+      Link.find({ categories: category })
+        .populate("postedBy", "_id name username")
+        .populate("categories", "name")
+        .sort({ createdAt: -1 })
+        .limit(limit)
+        .skip(skip)
+        .exec((err, links) => {
+          if (err) {
+            return res.status(400).json({
+              error: "Could not load links of a category",
+            });
+          }
+          res.json({ category, links });
+        });
+    });
 };
 
 exports.update = (req, res) => {
-  const {slug} = req.params
-  const {name, image, content} = req.body
+  const { slug } = req.params;
+  const { name, image, content } = req.body;
 
-  Category.findOneAndUpdate({slug}, {name, content}, {new: true}).exec((err, updated) => {
-    if(err) {
-      return res.status(400).json({
-        error : 'Could not find category to update'
-      });
-    }
-    console.log('UPDATED',updated)
-    if(image){
-      // remove the existing image from s3 before uploading new/updated one
-      const deleteParams = {
-        Bucket: "connect-content1",
-        Key: `${updated.image.key}`
-      };
-
-      s3.deleteObject(deleteParams, function(err, data){
-        if (err) {
-          console.log("S3 DELETE ERROR DURING UPDATE :", err);
-        }
-        else console.log('S3 DELETED DURING UPDATE',data)
-      })
-
-      const base64Data = new Buffer.from(
-        image.replace(/^data:image\/\w+;base64,/, ""),
-        "base64"
-      );
-      /* "^data:image" replaces with '' + ";base64," replaces with '' and  the type is 'base64' */
-    
-      const type = image.split(";")[0].split("/")[1];
-    
-
-      // upload image to s3
-      const params = {
-        Bucket: "connect-content1",
-        Key: `category/${uuidv4()}.${type}`,
-        Body: base64Data,
-        ACL: "public-read",
-        contentEncoding: "base64",
-        ContentType: `image/${type}`,
-      };
-
-      s3.upload(params, function (err, data) {
-        if (err) {
-          console.log("err :", err);
-          res.status(400).json({ error: "Upload to s3 failed" });
-        }
-    
-        console.log("AWS UPLOAD RES DATA", data);
-        updated.image.url = data.Location;
-        updated.image.key = data.key;
-    
-        // save to db
-        updated.save((err, success) => {
-          if (err) {
-            console.log("Error while post category: ", err);
-            res.status(400).json({ error: "Duplicate Category" });
-          }
-          res.json(success);  
+  Category.findOneAndUpdate({ slug }, { name, content }, { new: true }).exec(
+    (err, updated) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Could not find category to update",
         });
-      });
+      }
+      console.log("UPDATED ddd", updated);
+      if (image) {
+        // remove the existing image from s3 before uploading new/updated one
+        const deleteParams = {
+          Bucket: "connect-contentv2",
+          Key: `${updated.image.key}`,
+        };
 
+        s3.deleteObject(deleteParams, function (err, data) {
+          if (err) {
+            console.log("S3 DELETE ERROR DURING UPDATE :", err);
+          } else console.log("S3 DELETED DURING UPDATE", data);
+        });
 
+        const base64Data = new Buffer.from(
+          image.replace(/^data:image\/\w+;base64,/, ""),
+          "base64"
+        );
+        /* "^data:image" replaces with '' + ";base64," replaces with '' and  the type is 'base64' */
+
+        const type = image.split(";")[0].split("/")[1];
+
+        // upload image to s3
+        const params = {
+          Bucket: "connect-contentv2",
+          Key: `category/${uuidv4()}.${type}`,
+          Body: base64Data,
+          ACL: "public-read",
+          contentEncoding: "base64",
+          ContentType: `image/${type}`,
+        };
+
+        s3.upload(params, function (err, data) {
+          if (err) {
+            console.log("err :", err);
+            res.status(400).json({ error: "Upload to s3 failed" });
+          }
+
+          console.log("AWS UPLOAD RES DATA", data);
+          updated.image.url = data.Location;
+          updated.image.key = data.key;
+
+          // save to db
+          updated.save((err, success) => {
+            if (err) {
+              console.log("Error while post category: ", err);
+              res.status(400).json({ error: "Duplicate Category" });
+            }
+            res.json(success);
+          });
+        });
+      } else {
+        res.json(updated);
+      }
     }
-    else{   
-      res.json(updated)
-    }
-    
-  })
-
+  );
 };
 exports.remove = (req, res) => {
-  const {slug} = req.params;
+  const { slug } = req.params;
 
   Category.findOneAndRemove({ slug }).exec((err, data) => {
-    if(err) {
+    if (err) {
       return res.status(400).json({
-        error: 'Could not delete category'
+        error: "Could not delete category",
       });
     }
 
     // remove the existing image from s3 before uploading new/updated one
     const deleteParams = {
-      Bucket: "connect-content1",
-      Key: `${data.image.key}`
+      Bucket: "connect-contentv2",
+      Key: `${data.image.key}`,
     };
 
-    s3.deleteObject(deleteParams, function(err, data){
+    s3.deleteObject(deleteParams, function (err, data) {
       if (err) {
         console.log("S3 DELETE ERROR DURING UPDATE :", err);
-      }
-      else console.log('S3 DELETED DURING UPDATE',data); // deleted
-    })
+      } else console.log("S3 DELETED DURING UPDATE", data); // deleted
+    });
 
     res.json({
-      message: 'Category deleted successfully'
-    })
-  })
+      message: "Category deleted successfully",
+    });
+  });
 };
