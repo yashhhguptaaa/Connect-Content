6dc3a38cbc3145c0f7b495ead904cf1e89054b7e
Yash Gupta <yash3144@gmail.com>
2023-03-31 23:39:39 +0530

    security: updates the API

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    publicRuntimeConfig : {
-      APP_NAME: 'CONNECT-CONTENT',
-      API: 'http://localhost:8000/api',
-      PRODUCTION: false,
-      DOMAIN : 'http://localhost:3000',
-      FB_APP_ID:'JNCJNCSLSMKXSMXKSNXKNSK'
-    }
+  publicRuntimeConfig: {
+    APP_NAME: "CONNECT-CONTENT",
+    // API: "http://localhost:8000/api",
+    API: "https://connect-content-api.vercel.app/api",
+    PRODUCTION: false,
+    DOMAIN: "http://localhost:3000",
+    FB_APP_ID: "JNCJNCSLSMKXSMXKSNXKNSK",
+  },
 };
