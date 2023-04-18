02c5ac526167a44fe171d3a43fe53fc799a1ea38
Yash Gupta <yash3144@gmail.com>
2023-04-18 13:29:11 +0530

    feat: adds Poppins font globally

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -20,6 +20,10 @@ const Layout = ({ children }) => {
         crossOrigin="anonymous"
       />
       <link rel="stylesheet" href="/static/css/styles.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+			<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="/static/css/globals.css" />
     </React.Fragment>
   );
 
