5eb71a3565e7bcaa0918f54ed628f701f5c10be6
Tushar <69718699+tushar2k19@users.noreply.github.com>
2023-09-06 14:47:02 +0530

    Update link.js

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -5,6 +5,7 @@ const slugify = require("slugify");
 const AWS = require("aws-sdk");
 const { linkPublishedParams } = require("../helpers/email");
 
+
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
