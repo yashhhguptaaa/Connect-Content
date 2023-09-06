6847b4eda68b23a999e2023ca92173b27c294ce5
Tushar <69718699+tushar2k19@users.noreply.github.com>
2023-09-06 14:46:47 +0530

    Update category.js

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -5,7 +5,7 @@ const formidable = require("formidable");
 const AWS = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
 
-//s3
+
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
