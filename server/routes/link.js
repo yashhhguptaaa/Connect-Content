0eb854e26f7d68cdf6cb164d1e047b343d77661d
Tushar <69718699+tushar2k19@users.noreply.github.com>
2023-09-06 14:48:07 +0530

    Update link.js

--- server/routes/link.js
+++ server/routes/link.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const expressJwt = require("express-jwt");
 
-// Validators
+// Validator
 const {
   linkCreateValidator,
   linkUpdateValidator,
